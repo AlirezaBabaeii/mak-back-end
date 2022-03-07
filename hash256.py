4d7a64f521790d9d291ff11507c58c67d256546a
sisrsis <51427417+sisrsis@users.noreply.github.com>
2022-03-08 01:09:36 +0330

    نوشته شدن تابع چک کوکی

--- hash256.py
+++ hash256.py
@@ -11,5 +11,21 @@ def hashlogin(user,password):
     sql.execute(f"INSERT INTO hash (timehash,hashlogin,user) VALUES ('{datatime.date()}','{hase.hexdigest()}','{user}')")
     sql.commit()
     sql.close()
-
-    
+    hashlogin("virustest","111222")
+def gethash(hash,user):
+    sql=sqlite3.connect("sis.db")
+    data_t=sql.cursor()
+    data_t.execute(f"SELECT user, hashlogin, timehash FROM hash WHERE user = '{user}'")
+    da=data_t.fetchall()
+    if da[0][1] in hash:
+        print ("ok hash")
+        data=datetime.datetime.now()
+        if str(data.date()) in da[0][2]:
+            print("ok time")
+            return True
+        else:
+            print("error time")
+            return False
+    else :
+        print("error hash")
+        return False
