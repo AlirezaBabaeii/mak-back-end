8ddd4a41b547f7b8686223c84982a20038963d63
sisrsis <virussisrsis@gmail.com>
2022-03-10 16:39:00 +0330

    اضافه کردن تابع هایه ورود کاربر و ساخت کاربر با فایل دیتابیس

--- sql.py
+++ sql.py
@@ -15,3 +15,59 @@ def hashgetdb(user):
     data_t.execute(f"SELECT user, hashlogin, timehash FROM hash WHERE user = '{user}'")
     db=data_t.fetchall()
     return  db
+
+
+
+
+
+
+def data_create(email, password):
+    data_t=mydb.cursor()
+    a = data_t.execute(f"SELECT email, password FROM SAS WHERE email = '{email}';")
+    data_r=a.fetchall()
+    try :
+        if email in data_r[0]:
+            print("ok")
+    except:
+        data.execute(f"""
+        INSERT INTO SAS (
+                        password,
+                        email
+                    )
+                    VALUES (
+                        '{str(password)}',
+                        '{str(email)}'
+                    );
+        """)
+        print("ok test")
+        data.commit()
+    data.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+def data_login(email1,password):
+    data_t = mydb.cursor()
+    a = data_t.execute(f"SELECT email, password FROM SAS WHERE email = '{email1}';")
+    data_r = a.fetchall()
+    if email1 in data_r[0]:
+        print("ok")
+        if password in data_r[0][1]:
+            print(data_r[0][1])
+            print("ok password")
+            data.close()
+            return True
+        else :
+            print(data_r[0][1])
+            print("error")
+            data.close()
+            return False
