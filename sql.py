972d753a50209690d645ac6678d915b479f10c58
sisrsis <51427417+sisrsis@users.noreply.github.com>
2022-03-14 16:51:38 +0330

    اضافه شدن قسمت اضافه کردن هش ادمین به دیتابیس

--- sql.py
+++ sql.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import mysql.connector
-
+import hash256_admin
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -90,17 +90,13 @@ def admin_login_db(user,password):
     data_db.execute(f"SELECT user,pass,hashs,hashtime FROM admins WHERE user='{str(user)}'")
     data_r=data_db.fetchall()
     print(data_r)
-    try:
-        if data_r==user:
-            print("ok user")
-            if data_r[0][1]==password :
-                hase=hash_admin_login(user,password)
-                datatime=datetime.datetime.now()
-                data_db.execute(f'UPDATE admins SET hashs="{hase}",hashtime="{datatime}" WHERE user="{user}"')
-                data_db.commit()
-                return True
-    except:
-        return False
-                
-                
-admin_login_db("virussi1srsis@gmail.com","test11")
+    if data_r[0][0]==user:
+        print("ok user")
+        if data_r[0][1]==password:
+            print("ook password")
+            hase=hash256_admin.hash_admin_login(user,password)
+            datatime=datetime.now()
+            data_db.execute(f'UPDATE admins SET hashs="{hase}",hashtime="{datatime}" WHERE user="{user}"')
+            mydb.commit()
+            return str(hase)
+          
