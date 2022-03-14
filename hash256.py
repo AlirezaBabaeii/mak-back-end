bd34123606868d83bd14615e4fdb1b260597fa04
sisrsis <51427417+sisrsis@users.noreply.github.com>
2022-03-14 16:51:15 +0330

    قسمت هش کردن ادمین از اینجا برداشته شد

--- hash256.py
+++ hash256.py
@@ -32,13 +32,3 @@ def gethash(hash,user):
     
     
     
-    
-def hash_admin_login(user,password):
-    datatime = datetime.datetime.now()
-    hase=hashlib.sha256()
-    hase.update(str(user).encode())
-    hase.update(str(password).encode())
-    hase.update(str(user+user).encode())
-    hase.update(str(datatime.date()).encode())
-    return hase.hexdigest()
-
